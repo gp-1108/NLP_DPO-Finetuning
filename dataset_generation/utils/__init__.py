7a35a0c3b87214f631c0d5952a7ff554ba24e62b
gp-1108 <pietrogirotto00@gmail.com>
2024-10-15 10:04:27 +0200

    added a logger

--- dataset_generation/utils/__init__.py
+++ dataset_generation/utils/__init__.py
@@ -1,5 +1,6 @@
 from .TextExtractor import TextExtractor
 from .DialogueGenerator import DialogueGenerator
 from .DPOGenerator import DPOGenerator
+from .my_logger import Logger, log_function_call
 
-__all__ = ["TextExtractor", "DialogueGenerator", "DPOGenerator"]
+__all__ = ["TextExtractor", "DialogueGenerator", "DPOGenerator", "Logger", "log_function_call"]
