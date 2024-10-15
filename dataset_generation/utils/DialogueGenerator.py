370c881c509b7cd832e4ed57a3dfe13114e6e2f0
gp-1108 <pietrogirotto00@gmail.com>
2024-10-15 09:46:07 +0200

    moved to whole json folder processing

--- dataset_generation/utils/DialogueGenerator.py
+++ dataset_generation/utils/DialogueGenerator.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 from pydantic import BaseModel
 import os
+import json
+from tqdm import tqdm
 
 class InteractionSchema(BaseModel):
     student_question: str
@@ -25,7 +27,110 @@ class DialogueGenerator:
         self.model = model
         self.json_files = DialogueGenerator._load_jsons(jsons_path)
         self.output_dir = output_dir
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
         self.prompt = open(prompt_path, "r").read() # The prompt with the <SOURCE_TEXT> token to be replaced
+    
+    def generate_dialogues(self):
+        """
+        This function will generate dialogues for all json files.
+        """
+        for json_file in tqdm(self.json_files, desc="Processing JSON files"):
+            text_chunks = self._load_text_chunks(json_file)
+            source_texts = self._define_source_texts(text_chunks)
+            dialogue = []
+            for source_text in source_texts:
+                dialogue += self.generate_dialogue(source_text)
+            new_json_path = os.path.join(self.output_dir, os.path.basename(json_file))
+            self._save_dialogue(dialogue, new_json_path)
+
+    def _define_source_texts(self, text_chunks: list[str]) -> list[str]:
+        """
+        This function will define the source texts for the dialogues given the original text chunks
+        of the pdf file (extracted with the TextExtractor class).
+
+        Args:
+            text_chunks (list[str]): A list of text chunks.
+
+        Returns:
+            list[str]: A list of source texts, combining the text chunks.
+        """
+        if not text_chunks:
+            return []
+        # [TODO] Carefully think about this function as it is crucial for the quality of the dialogues
+
+        
+        # 1. Merge chunks until the length is around 5K chars
+        # source_texts = []
+        # source_text = text_chunks[0]
+        # for chunk in text_chunks[1:]:
+        #     if len(source_text) + len(chunk) > 5000:
+        #         source_texts.append(source_text)
+        #         source_text = chunk
+        #     else:
+        #         source_text += chunk
+        #         source_texts.append(source_text)
+
+        # 2. Merge chunks with length around 3K but do overlaps from left and right of 1K
+        source_texts = []
+        start, end = 0, 0
+        length = 0
+        while end < len(text_chunks):
+            # First let's check that we have a text longer than 5K
+            length += len(text_chunks[end])
+            if length > 5000:
+                # Now we add the the overlapping chunks from the left and right
+                if start > 0:
+                    source_text = text_chunks[start-1][-1000:]
+                source_text = text_chunks[start]
+                for i in range(start+1, end+1):
+                    source_text += text_chunks[i]
+                if end < len(text_chunks) - 1:
+                    source_text += text_chunks[end+1][-1000:]
+                source_texts.append(source_text)
+                start, end = end+1, end+1
+                length = 0
+            else:
+                end += 1
+        return source_texts
+                
+        
+    def _load_text_chunks(self, json_path: str) -> list[str]:
+        """
+        This function will load the text chunks from a json file.
+
+        Args:
+            json_path (str): The path to the json file.
+
+        Returns:
+            list[str]: A list of text chunks.
+        """
+        with open(json_path, 'r') as f:
+            return json.load(f)
+    
+    def _save_dialogue(self, dialogue: list[dict], json_path: str):
+        """
+        This function will save the dialogue in a json file.
+
+        Args:
+            dialogue (list[dict]): The dialogue to be saved.
+            json_path (str): The path to the json file.
+        """
+        with open(json_path, 'w') as f:
+            json.dump(dialogue, f)
+
+
+    def generate_dialogue(self, source_text: str) -> list[dict]:
+        """
+        This function will generate a dialogue based on the source text.
+
+        Args:
+            source_text (str): The source text to be used in the prompt.
+
+        Returns:
+            list[dict]: A list of dictionaries with the format {"student_question": str, "tutor_response": str}
+        """
+        return self._query_openai(source_text)
     
     def _query_openai(self, source_text: str) -> list[dict]:
         """
--- dataset_generation/utils/TextExtractor.py
+++ dataset_generation/utils/TextExtractor.py
@@ -2,6 +2,8 @@ from pypdf import PdfReader
 import re
 import json
 from unidecode import unidecode
+import os
+import tqdm
 
 class TextExtractor:
     EMAIL_TOKEN = "<EMAIL_TOKEN>"
@@ -10,160 +12,219 @@ class TextExtractor:
     PUNCTUATION_TOKEN = "<PUNCTUATION_TOKEN>"
     CHUNK_MIN_LENGTH = 1000
     CHUNK_MAX_LENGTH = 7000
-    
-    def __init__(self, pdf_path: str):
-        self.pdf_path = pdf_path
-        file = PdfReader(pdf_path)
-        self.text = ""
-        for page in file.pages:
-            self.text += page.extract_text()
-    
-    def _pre_process(self):
-        # Removing unicode characters
-        self.text = unidecode(self.text)
-        # Removing interfering punctuation
-        self._remove_emails()
-        self._remove_urls()
-        self._remove_small_words(3)
 
-        # Preparing the text for chunking
-        self._remove_punctuation()
-        
-        # Re-adding the removed punctuation
-        self._add_small_words()
-        self._add_urls()
-        self._add_emails()
-        
-        chunks = self.text.split(self.PUNCTUATION_TOKEN)
-        chunks = [chunk for chunk in chunks if chunk]
+    def __init__(self, pdfs_path: str, output_dir: str):
+        self.pdf_files = TextExtractor._load_pdf_files(pdfs_path)
+        self.output_dir = output_dir
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+    def extract_texts(self):
+        """
+        Extracts text from all PDF files, processes it, and saves the chunks to the output directory.
+        """
+        for pdf_file in tqdm.tqdm(self.pdf_files, desc="Extracting text from PDFs"):
+            reader = PdfReader(pdf_file)
+            text = ""
+            for page in reader.pages:
+                page_text = page.extract_text()
+                if page_text:
+                    text += page_text
+            if text:
+                self.save_text(text, os.path.join(self.output_dir, os.path.basename(pdf_file).replace(".pdf", ".json")))
+
+    @staticmethod
+    def _load_pdf_files(pdfs_path: str) -> list[str]:
+        """
+        Loads all PDF file paths from the given directory.
+        """
+        pdf_files = []
+        for root, _, filenames in os.walk(pdfs_path):
+            for filename in filenames:
+                if filename.endswith(".pdf"):
+                    pdf_files.append(os.path.join(root, filename))
+        return pdf_files
+
+    def _pre_process(self, text: str) -> list[str]:
+        """
+        Pre-processes the text by removing emails, URLs, small words, and punctuation,
+        then re-adds them after processing.
+        """
+        # Removing unicode characters
+        text = unidecode(text)
+
+        # Remove references
+        text = self._remove_references(text)
+
+        # Removing interfering elements
+        text, emails = self._remove_emails(text)
+        text, urls = self._remove_urls(text)
+        text, small_words = self._remove_small_words(text, 4)
+        text, punctuation = self._remove_punctuation(text)
+
+        # Re-adding the removed elements
+        text = self._add_small_words(text, small_words)
+        text = self._add_urls(text, urls)
+        text = self._add_emails(text, emails)
+
+        chunks = text.split(self.PUNCTUATION_TOKEN)
 
         # Adding back the punctuation that was removed
         for i in range(len(chunks)):
-            chunks[i] = chunks[i] + (self.punctuation.pop(0) if self.punctuation else "")
-        
+            if i < len(punctuation):
+                chunks[i] = chunks[i] + punctuation[i]
+
         # Unifying chunks that are too small
         chunks = self._unify_chunks(chunks)
-
         chunks = self._polish_chunks(chunks)
 
-        self.chunks = chunks
-        return chunks 
-    
-    def _polish_chunks(self, chunks: list[str]) -> list[str]:
-        """
-        This function will polish the chunks by removing any \n in the phrases
-        and removing any leading or trailing spaces (or unicode characters).
-        """
-        for i in range(len(chunks)):
-            chunks[i] = chunks[i].replace("\n", " ")
-            chunks[i] = chunks[i].strip()
         return chunks
 
-    
-    def get_chunks(self):
+    def _polish_chunks(self, chunks: list[str]) -> list[str]:
         """
-        This function will return the chunks of text.
-        """
-        return self.chunks
-    
-    def _unify_chunks(self, chunks: list[str]) -> list[str]:
-        """
-        This function will merge subsequent chunks that are too small or if the chunks ends/starts
-        with \n or space.
+        This function will polish the chunks by removing not useful chunks based on some criteria.
+        It will return a list of polished chunks.
 
         Args:
             chunks (list[str]): A list of text chunks.
-        
+
         Returns:
-            list[str]: A list of text chunks with some of them merged.
+            list[str]: A list of polished text chunks.
+        """
+        polished_chunks = []
+        for chunk in chunks:
+            chunk = chunk.replace("\n", "").strip()
+
+            # If the content of alphanumerical characters is less than 50% of the chunk
+            # then we will discard the chunk.
+            if not chunk or len(re.sub(r"[^a-zA-Z0-9]", "", chunk)) / len(chunk) < 0.5:
+                continue
+            polished_chunks.append(chunk)
+        return polished_chunks
+    
+    def _remove_references(self, text: str) -> str:
+        """
+        Removes references from the text.
+
+        Args:
+            text (str): The text to remove references from.
+
+        Returns:
+            str: The text without references.
+        """
+        # First we need to know how many times the "references" word appears in the text
+        # if it is more than 10, it means that the text has multiple references.
+        # as of right now this is not supported and the text will be returned as is.
+        search_text = text.lower()
+        references_count = search_text.count("references")
+        if references_count > 10 or references_count == 0:
+            return text
+        
+        # Now we need to find the index at which the "references word appears".
+        # To do it we will progressively look at " references " " references" "references"
+        # whichever matches first will be the index at which the references word appears.
+        # we will always take the last match as the references are usually at the end of the text.
+        references_index = -1
+        if " references " in search_text:
+            references_index = search_text.rindex(" references ")
+        elif " references" in search_text:
+            references_index = search_text.rindex(" references")
+        elif "references" in search_text:
+            references_index = search_text.rindex("references")
+        
+        return text[:references_index]
+    
+
+    def _unify_chunks(self, chunks: list[str]) -> list[str]:
+        """
+        Merges subsequent chunks that are too small or improperly split.
         """
         if len(chunks) <= 1:
             return chunks
-            
+
         unified_chunks = [chunks[0]]
-        matches = ["\n", "\t"]
+        matches = ["\n", "\t", " "]
 
         for i in range(1, len(chunks)):
-            if (len(unified_chunks[-1]) < self.CHUNK_MIN_LENGTH \
-                or chunks[i][0] in matches \
-                or chunks[i-1][-1] in matches) \
-                and len(unified_chunks[-1]) + len(chunks[i]) < self.CHUNK_MAX_LENGTH:
-                unified_chunks[-1] += chunks[i]
+            last_chunk = unified_chunks[-1]
+            current_chunk = chunks[i]
+            if (len(last_chunk) < self.CHUNK_MIN_LENGTH or
+                current_chunk.startswith(tuple(matches)) or
+                last_chunk.endswith(tuple(matches))) and \
+                len(last_chunk) + len(current_chunk) < self.CHUNK_MAX_LENGTH:
+                unified_chunks[-1] += ' ' + current_chunk
             else:
-                unified_chunks.append(chunks[i])
+                unified_chunks.append(current_chunk)
         return unified_chunks
-    
-    def _remove_punctuation(self):
-        """
-        This function will remove all punctuation from the text.
-        """
-        # Define punctuation to be removed: . , ! ? ;
-        self.punctuation = re.findall(r"[.!?;]", self.text)
-        self.text = re.sub(r"[.!?;]", self.PUNCTUATION_TOKEN, self.text)
-    
-    def _add_punctuation(self):
-        """
-        This function will add the punctuation that was removed.
-        """
-        while self.PUNCTUATION_TOKEN in self.text and self.punctuation:
-            self.text = self.text.replace(self.PUNCTUATION_TOKEN, self.punctuation.pop(0), 1)
 
-    def _remove_small_words(self, max_length: int):
+    def _remove_punctuation(self, text: str):
         """
-        This function will remove all small words that end with a period or are part of 
-        abbreviations like 'et al.' or 'e.g.'.
+        Removes punctuation from the text.
         """
-        # Match small words followed by period and any other characters (like space or more text)
-        small_word_regex = r"\b\w{0," + str(max_length) + r"}\.(?!\n)(?:\s|[^a-zA-Z\n]|\b\w{1}\.)*"
-        self.small_words = re.findall(small_word_regex, self.text)
-        self.text = re.sub(small_word_regex, self.SMALL_WORDS_TOKEN, self.text)
+        punctuation = re.findall(r"[.!?;]", text)
+        text = re.sub(r"[.!?;]", self.PUNCTUATION_TOKEN, text)
+        return text, punctuation
 
-    
-    def _add_small_words(self):
+    def _remove_small_words(self, text: str, max_length: int):
         """
-        This function will add the small words that were removed.
+        Removes small words that could interfere with chunking.
         """
-        while self.SMALL_WORDS_TOKEN in self.text and self.small_words:
-            self.text = self.text.replace(self.SMALL_WORDS_TOKEN, self.small_words.pop(0), 1)
-        
-    def _remove_emails(self):
+        small_word_regex = r"\b\w{1," + str(max_length) + r"}\.(?!\n)"
+        small_words = re.findall(small_word_regex, text)
+        text = re.sub(small_word_regex, self.SMALL_WORDS_TOKEN, text)
+        return text, small_words
+
+    def _remove_emails(self, text: str):
         """
-        This function will replace all emails with the special token EMAIL and
-        save the emails in a list to later replace the token with the email once
-        the chunks are divided by the stop words.
-        """ 
-        email_regex = r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}"
-        self.emails = re.findall(email_regex, self.text)
-        self.text = re.sub(email_regex, self.EMAIL_TOKEN, self.text)
-    
-    def _add_emails(self):
+        Replaces emails with a token and stores them for later re-insertion.
         """
-        This function will replace the EMAIL token with the email.
+        email_regex = r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+"
+        emails = re.findall(email_regex, text)
+        text = re.sub(email_regex, self.EMAIL_TOKEN, text)
+        return text, emails
+
+    def _remove_urls(self, text: str):
         """
-        while self.EMAIL_TOKEN in self.text and self.emails:
-            self.text = self.text.replace(self.EMAIL_TOKEN, self.emails.pop(0), 1)
-    
-    def _remove_urls(self): 
+        Replaces URLs with a token and stores them for later re-insertion.
         """
-        This function will replace all URLs with the special token URL and
-        save the URLs in a list to later replace the token with the URL once
-        the chunks are divided by the stop words.
+        url_regex = r"https?://[^\s]+"
+        urls = re.findall(url_regex, text)
+        text = re.sub(url_regex, self.URL_TOKEN, text)
+        return text, urls
+
+    def _add_emails(self, text: str, emails: list):
         """
-        url_regex = r"https?://(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:/[^\\s]*)?"
-        self.urls = re.findall(url_regex, self.text)
-        self.text = re.sub(url_regex, self.URL_TOKEN, self.text)
-    
-    def _add_urls(self):
+        Replaces email tokens with the original emails.
         """
-        This function will replace the URL token with the URL.
+        for email in emails:
+            text = text.replace(self.EMAIL_TOKEN, email, 1)
+        return text
+
+    def _add_urls(self, text: str, urls: list):
         """
-        while self.URL_TOKEN in self.text and self.urls:
-            self.text = self.text.replace(self.URL_TOKEN, self.urls.pop(0), 1)
-    
-    def save_text(self, json_path: str):
+        Replaces URL tokens with the original URLs.
         """
-        This function will save the text in a json file.
+        for url in urls:
+            text = text.replace(self.URL_TOKEN, url, 1)
+        return text
+
+    def _add_small_words(self, text: str, small_words: list):
         """
-        chunks = self._pre_process()
-        with open(json_path, 'w') as f:
-            json.dump(chunks, f, indent=2)
+        Replaces small word tokens with the original small words.
+        """
+        for word in small_words:
+            text = text.replace(self.SMALL_WORDS_TOKEN, word, 1)
+        return text
+
+    def save_text(self, text: str, json_path: str):
+        """
+        Saves the processed text chunks into a JSON file.
+        """
+        chunks = self._pre_process(text)
+
+        if not chunks or sum(len(chunk.split()) for chunk in chunks) < 200:
+            print(f"Text for file {json_path} is probably corrupted or not useful.")
+            return
+
+        with open(json_path, 'w', encoding='utf-8') as f:
+            json.dump(chunks, f, indent=2, ensure_ascii=False)
