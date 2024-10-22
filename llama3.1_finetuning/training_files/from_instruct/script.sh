2083b9d6b3123584a296bb9be7c0d49d4774247d
gp-1108 <pietrogirotto00@gmail.com>
2024-10-22 13:51:53 +0200

    way more modular now

--- llama3.1_finetuning/training_files/from_instruct/script.sh
+++ llama3.1_finetuning/training_files/from_instruct/script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-project_dir="/home/gp1108/Code/Thesis/llama3.1_finetuning/training_files/from_instruct"
-script_path="${project_dir}/training_script_Instruct.py"
-train_data="/home/gp1108/Code/Thesis/llama3.1_finetuning/dataset_preparation/converted_datasets/final_dev_set.json"
-dev_data="/home/gp1108/Code/Thesis/llama3.1_finetuning/dataset_preparation/converted_datasets/final_dev_set.json"
+base_path="/home/gp1108/Code/Thesis/llama3.1_finetuning"
+project_dir="${base_path}/training_files/from_instruct"
+script_path="${project_dir}/training_script_Base.py"
+train_data="${base_path}/dataset_preparation/converted_datasets/final_train_set.json"
+dev_data="${base_path}/dataset_preparation/converted_datasets/final_dev_set.json"
 output_path="${project_dir}/output"
-sif_image_path="/home/gp1108/Code/Thesis/llama3.1_finetuning/training_files/env.sif"
+sif_image_path="${base_path}/training_files/env.sif"
 run_name="llama3.1_SFT_from_Instruct"
-load_4_bit=False
+load_8_bit=True
 max_seq_len=120000
 
 HF_TOKEN="<YOUR_HUGGING_FACE_TOKEN>"
