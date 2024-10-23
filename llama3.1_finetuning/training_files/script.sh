645fed523d640cc06d2e87948e0aa67f2afaba72
gp-1108 <pietrogirotto00@gmail.com>
2024-10-23 11:16:15 +0200

    final script

--- llama3.1_finetuning/training_files/script.sh
+++ llama3.1_finetuning/training_files/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-base_path="/home/gp1108/Code/Thesis/llama3.1_finetuning"
+my_home_folder="/nfsd/nldei/girottopie"
+base_path="/nfsd/nldei/girottopie/llama3.1_finetuning"
 project_dir="${base_path}/training_files"
 script_path="${project_dir}/training_script_Base.py"
 train_data="${base_path}/dataset_preparation/converted_datasets/final_train_set.json"
@@ -8,13 +9,14 @@ dev_data="${base_path}/dataset_preparation/converted_datasets/final_dev_set.json
 output_path="${project_dir}/output"
 sif_image_path="${project_dir}/env.sif"
 run_name="llama3.1_SFT_from_Instruct"
-load_8_bit=True
+load_8_bit="True"
 max_seq_len=120000
 
 export HF_TOKEN="your token"
+export HF_HOME="${my_home_folder}/.cache/"
 
 cd $project_dir
-singularity exec --nv -B $base_path $sif_image_path \
+singularity exec --nv --no-home -B $my_home_folder $sif_image_path \
     python3 $script_path \
     --ds_train $train_data \
     --ds_dev $dev_data \
