c4d91f99df5777507bcb475c4692bf6b75156fca
gp-1108 <pietrogirotto00@gmail.com>
2024-10-27 15:37:52 +0100

    added chunk entity

---
+++ dataset_generation/core/components/BaseSubComponent.py
@@ -0,0 +1,10 @@
+class BaseSubComponent:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+    
+    def to_json_str(self):
+        raise NotImplementedError
+    
+    def from_json_str(self, json_str: str):
+        raise NotImplementedError
---
+++ dataset_generation/core/components/Chunk.py
@@ -0,0 +1,42 @@
+from .BaseSubComponent import BaseSubComponent
+import json
+
+class Chunk(BaseSubComponent):
+    def __init__(self,
+                 id: int=None,
+                 doc_id: str=None,
+                 text: str=None,
+                 json_str: str = None
+                ):
+        if json_str:
+            self.from_json_str(json_str)
+        else:
+            if  not id or \
+                not doc_id or \
+                not text:
+                raise Exception("Chunk: Missing required parameters")
+            super().__init__(
+                id=id,
+                doc_id=doc_id,
+                text=text
+            )
+    
+    @staticmethod
+    def extract_ids(chunk_id: str):
+        doc_id, id = chunk_id.split("_ch_")
+        return doc_id, int(id)
+    
+    def get_id(self):
+        return f"{self.doc_id}_ch_{self.id}"
+    
+    def to_json_str(self):
+        return json.dumps({
+            "id": self.get_id(),
+            "text": self.text
+        })
+    
+    def from_json_str(self, json_str: str):
+        data = json.loads(json_str)
+        self.id = data["id"]
+        self.doc_id, self.id = Chunk.extract_ids(self.id)
+        self.text = data["text"]
