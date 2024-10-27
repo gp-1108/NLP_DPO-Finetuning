f09d7d322eca0b0e80a650b2f80bfba855c9bcc1
gp-1108 <pietrogirotto00@gmail.com>
2024-10-27 16:12:04 +0100

    DPO dialogue created

---
+++ dataset_generation/core/components/DPODialogue.py
@@ -0,0 +1,37 @@
+from .DPOTurn import DPOTurn
+from .BaseComponent import BaseComponent
+import json
+
+class DPODialogue(BaseComponent):
+    def __init__(self,
+                 dialogue_id: str=None,
+                 dpo_id: int=None,
+                 turns: list[DPOTurn]=None,
+                 json_str: str = None
+                ):
+        if json_str:
+            self.from_json_str(json_str)
+        else:
+            if  not dialogue_id or \
+                not dpo_id or \
+                not turns:
+                raise Exception("DPODialogue: Missing required parameters")
+            super().__init__(
+                dialogue_id=dialogue_id,
+                dpo_id=dpo_id,
+                turns=turns
+            )
+
+    def get_id(self):
+        return f"{self.dialogue_id}_dpo{self.dpo_id}"
+    
+    def to_json_str(self):
+        return json.dumps({
+            "id": self.get_id(),
+            "turns": [turn.to_json_str() for turn in self.turns]
+        })
+
+    def from_json_str(self, json_str: str):
+        data = json.loads(json_str)
+        self.dialogue_id, self.dpo_id = DPODialogue.extract_ids(data["id"])
+        self.turns = [DPOTurn(json_str=json.dumps(turn)) for turn in data["turns"]]
---
+++ dataset_generation/core/components/DPOTurn.py
@@ -0,0 +1,42 @@
+from .BaseSubComponent import BaseSubComponent
+import json
+
+class DPOTurn(BaseSubComponent):
+    def __init__(self,
+                 role: str=None,
+                 positive_sample: bool=None,
+                 negative_sample: bool=None,
+                 rule_used: int=None,
+                 json_str: str = None
+                ):
+        if json_str:
+            self.from_json_str(json_str)
+        else:
+            if  not role or \
+                not positive_sample or \
+                not negative_sample or \
+                not rule_used:
+                raise Exception("DPOTurn: Missing required parameters")
+            if positive_sample and negative_sample:
+                raise Exception("DPOTurn: positive_sample and negative_sample can't be both True")
+            super().__init__(
+                role=role,
+                positive_sample=positive_sample,
+                negative_sample=negative_sample,
+                rule_used=rule_used
+            )
+    
+    def to_json_str(self):
+        return json.dumps({
+            "role": self.role,
+            "positive_sample": self.positive_sample,
+            "negative_sample": self.negative_sample,
+            "rule_used": self.rule_used
+        })
+    
+    def from_json_str(self, json_str: str):
+        data = json.loads(json_str)
+        self.role = data["role"]
+        self.positive_sample = data["positive_sample"]
+        self.negative_sample = data["negative_sample"]
+        self.rule_used = data["rule_used"]
