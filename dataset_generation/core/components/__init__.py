096ca0bc013fc189aac9cc362225a554cacdb19e
gp-1108 <pietrogirotto00@gmail.com>
2024-10-27 16:12:55 +0100

    finalized package

---
+++ dataset_generation/core/components/__init__.py
@@ -0,0 +1,15 @@
+from .Chunk import Chunk
+from .Document import Document
+from .Dialogue import Dialogue
+from .Turn import Turn
+from .DPODialogue import DPODialogue
+from .DPOTurn import DPOTurn
+
+__all__ = [
+    "Chunk",
+    "Document",
+    "Dialogue",
+    "Turn",
+    "DPODialogue",
+    "DPOTurn"
+]
