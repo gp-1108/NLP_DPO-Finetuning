99f61ce9f3e43c292fae65cbbd5ca3beed088d9f
gp-1108 <pietrogirotto00@gmail.com>
2024-11-08 16:17:09 +0100

    Added dialogue generator

--- dataset_generation/core/processes/__init__.py
+++ dataset_generation/core/processes/__init__.py
@@ -1,6 +1,7 @@
-from .TextExtractor import TextExtractor
+from .ChunkExtractor import ChunkExtractor
 from .DialogueGenerator import DialogueGenerator
-from .DPOGenerator import DPOGenerator
-from .my_logger import Logger, log_function_call
 
-__all__ = ["TextExtractor", "DialogueGenerator", "DPOGenerator", "Logger", "log_function_call"]
+__all__ = [
+    "ChunkExtractor",
+    "DialogueGenerator"
+]
