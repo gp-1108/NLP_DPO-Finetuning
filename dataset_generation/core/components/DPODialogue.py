aaab5a9961bd57254c3fe7d1928fb39cb00c55eb
gp-1108 <pietrogirotto00@gmail.com>
2024-11-14 17:39:07 +0100

    moved to saving solely the last turn

--- dataset_generation/core/components/DPODialogue.py
+++ dataset_generation/core/components/DPODialogue.py
@@ -5,39 +5,38 @@ import json
 class DPODialogue(BaseComponent):
     def __init__(self,
                  id: str=None,
-                 turns: list[DPOTurn]=None,
-                 json_str: str = None
+                 last_turn: DPOTurn=None,
+                 json_str: str = None,
+                 output_jsonl: str = None,
                 ):
         if json_str:
             self.from_json_str(json_str)
         else:
             if id is None or \
-               turns is None:
+               last_turn is None or \
+                output_jsonl is None:
                 raise Exception("DPODialogue: Missing required parameters")
             super().__init__(
+                output_file=output_jsonl,
                 id = id,
-                turns=turns
+                last_turn=last_turn
             )
 
     @staticmethod
-    def get_id(dialogue_id: str, dpo_int_id: int):
-        return f"{dialogue_id}_dpo{dpo_int_id}"
-    
-    @staticmethod
-    def extract_ids(dialogue_id: str):
-        doc_id, id = dialogue_id.split("_dpo")
-        return doc_id, int(id)
+    def get_id(dialogue_id: str, rules_idx_used: list[int]):
+        return f"{dialogue_id}_dpo[{'_'.join([str(idx) for idx in rules_idx_used])}]"
     
     def to_json_str(self):
         return json.dumps({
-            "id": self.get_id(),
-            "turns": [turn.to_json_str() for turn in self.turns]
+            "id": self.id,
+            "last_turn": self.last_turn.to_json_str()
         })
 
     def from_json_str(self, json_str: str):
         data = json.loads(json_str)
-        self.dialogue_id, self.dpo_id = DPODialogue.extract_ids(data["id"])
-        self.turns = [DPOTurn(json_str=json.dumps(turn)) for turn in data["turns"]]
+        self.id = data["id"]
+        self.last_turn = DPOTurn(json_str=data["last_turn"])
     
     def __str__(self):
-        string = f"Dialogue ID: {self.dialogue_id}\n"
+        string = f"Dialogue ID: {self.id}\n"
+        return string
