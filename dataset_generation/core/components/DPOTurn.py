303cbfa459aec53264de0992804f43341bf58e84
gp-1108 <pietrogirotto00@gmail.com>
2024-11-14 17:39:32 +0100

    now a turn is both question and answer

--- dataset_generation/core/components/DPOTurn.py
+++ dataset_generation/core/components/DPOTurn.py
@@ -3,46 +3,52 @@ import json
 
 class DPOTurn(BaseSubComponent):
     def __init__(self,
-                 role: str=None,
-                 positive_sample: bool=None,
-                 negative_sample: bool=None,
+                 student_question: str=None,
+                 positive_answer: str=None,
+                 negative_answer: str=None,
                  rule_used: int=None,
                  json_str: str = None
                 ):
         if json_str:
             self.from_json_str(json_str)
         else:
-            if role is None or \
-                positive_sample is None or \
-                negative_sample is None or \
+            if student_question is None or \
+                positive_answer is None or \
+                negative_answer is None or \
                 rule_used is None:
                 raise Exception("DPOTurn: Missing required parameters")
-            if positive_sample and negative_sample:
-                raise Exception("DPOTurn: positive_sample and negative_sample can't be both True")
             super().__init__(
-                role=role,
-                positive_sample=positive_sample,
-                negative_sample=negative_sample,
+                student_question=student_question,
+                positive_answer=positive_answer,
+                negative_answer=negative_answer,
                 rule_used=rule_used
             )
     
     def to_json_str(self):
         return json.dumps({
-            "role": self.role,
-            "positive_sample": self.positive_sample,
-            "negative_sample": self.negative_sample,
-            "rule_used": self.rule_used
+            "positive_answer": self.positive_answer,
+            "negative_answer": self.negative_answer,
+            "rule_used": self.rule_used,
+            "student_question": self.student_question
         })
     
     def from_json_str(self, json_str: str):
         data = json.loads(json_str)
-        self.role = data["role"]
-        self.positive_sample = data["positive_sample"]
-        self.negative_sample = data["negative_sample"]
+        self.positive_answer = data["positive_answer"]
+        self.negative_answer = data["negative_answer"]
         self.rule_used = data["rule_used"]
+        self.student_question = data["student_question"]
     
     def __str__(self):
-        string = f"Role: {self.role}\n"
-        string += f"Rule Used: {self.rule_used}"
-        string += " (Positive Sample)" if self.positive_sample else " (Negative Sample)"
+        string = f"Student: {self.student_question[:10]}...\n"
+        string += f"Rule Used: {self.rule_used}\n"
+        string += f"Positive answer: {self.positive_answer[:10]}...\n"
+        string += f"Negative answer: {self.negative_answer[:10]}...\n"
+        return string
+    
+    def __repr__(self):
+        string = f"Rule Used: {self.rule_used}\n"
+        string += f"Student: {self.student_question}\n"
+        string += f"Positive answer: {self.positive_answer}\n"
+        string += f"Negative answer: {self.negative_answer}\n"
         return string
--- dataset_generation/core/components/Turn.py
+++ dataset_generation/core/components/Turn.py
@@ -3,32 +3,39 @@ import json
 
 class Turn(BaseSubComponent):
     def __init__(self,
-                 role: str=None,
-                 content: str=None,
+                 user: str=None,
+                 assistant: str=None,
                  json_str: str = None
                 ):
         if json_str:
             self.from_json_str(json_str)
         else:
-            if role is None or content is None:
+            if user is None or assistant is None:
                 raise ValueError("You either load the file from json_str or provide role, content")
 
             super().__init__(
-                role=role,
-                content=content
+                user=user,
+                assistant=assistant
             )
     
     def to_json_str(self):
         return json.dumps({
-            "role": self.role,
-            "content": self.content
+            "user": self.user,
+            "assistant": self.assistant
         })
     
     def from_json_str(self, json_str):
         data = json.loads(json_str)
-        self.role = data["role"]
-        self.content = data["content"]
+        self.user = data["user"]
+        self.assistant = data["assistant"]
+        
     
     def __str__(self):
-        string = f"Role: {self.role}\n"
-        string += f"Content: {self.content[:10]}...\n"
+        string = f"User: {self.user[:10]}...\n"
+        string += f"Assistant: {self.assistant[:10]}...\n"
+        return string
+    
+    def __repr__(self):
+        string = f"User: {self.user}\n"
+        string += f"Assistant: {self.assistant}\n"
+        return string
