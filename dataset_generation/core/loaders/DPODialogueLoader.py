4b34a3a610022f9636e69802fb2dd8b6c401ba98
gp-1108 <pietrogirotto00@gmail.com>
2024-11-14 17:42:19 +0100

    handled if file does not exist

--- dataset_generation/core/loaders/DPODialogueLoader.py
+++ dataset_generation/core/loaders/DPODialogueLoader.py
@@ -1,11 +1,14 @@
 from ..components.DPODialogue import DPODialogue
 from .BaseLoader import BaseLoader
+import os
 
 class DPODialogueLoader(BaseLoader):
     def __init__(self, jsonl_path):
         super().__init__(jsonl_path)
     
     def load_data(self):
+        if not os.path.exists(self.jsonl_path):
+            return []
         with open(self.jsonl_path, 'r') as file:
             data = [DPODialogue(json_str=line) for line in file if line.strip()]
         return data
