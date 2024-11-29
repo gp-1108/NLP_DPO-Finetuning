4c8d2321692f6cfc5d68892f895777fb6a6c796d
gp-1108 <pietrogirotto00@gmail.com>
2024-11-29 10:22:22 +0100

    added data visuals

--- start_data_vis.sh
+++ start_data_vis.sh
@@ -1,5 +1,14 @@
+# Updating the repo
 git pull
 
+# Stopping container running dpo_webapp:thesis
+docker stop $(docker ps -a -q --filter "ancestor=dpo_webapp:thesis")
+
+# Removing container running dpo_webapp:thesis
+docker rm $(docker ps -a -q --filter "ancestor=dpo_webapp:thesis")
+
+# Rebuiding the docker image
 docker build -t dpo_webapp:thesis -f dataset_visualization/Dockerfile .
 
+# Running the docker container
 docker run -d -v /certs:/certs -p 5005:5005 dpo_webapp:thesis
