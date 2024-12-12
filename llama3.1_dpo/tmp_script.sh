4248f7f20d75c851772345f55d3f1891565aeb84
gp-1108 <pietrogirotto00@gmail.com>
2024-12-12 19:29:33 +0100

    latest version of tmp_script

--- llama3.1_dpo/tmp_script.sh
+++ llama3.1_dpo/tmp_script.sh
@@ -1,10 +1,12 @@
-export HF_TOKEN="<huggingface token>"
+export HF_TOKEN="<token here>"
 export HF_HOME="/nfsd/nldei/girottopie/.cache"
-export WANDB_API_KEY="<wandb key>"
+export WANDB_API_KEY="<token here>"
 
-accelerate launch --num_processes=1 dpo_finetuning.py \
+accelerate launch --num_processes=2 --num_machines=1 --mixed_precision=no --dynamo_backend=inductor dpo_finetuning.py \
     --dataset_path ../dataset_generation/data/dpo_dialogues.jsonl \
     --peft_model_id ../llama3.1_finetuning/output/llama3.1_SFT_from_Base/checkpoint-800 \
     --output_dir ./tmp \
+    --logging_steps 2 \
     --load_in_8bit \
-    --batch_size 2
+    --batch_size 1 \
+    --gradient_acc 4
