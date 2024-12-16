0bd1703cbdfb7db8f410146b4465edc10a9b7f8d
gp-1108 <pietrogirotto00@gmail.com>
2024-12-16 14:55:16 +0100

    added MAD filtering

--- llama3.1_dpo/utils.py
+++ llama3.1_dpo/utils.py
@@ -1,5 +1,6 @@
 from core.loaders import DPODialogueLoader
 from datasets import Dataset
+import numpy as np
 
 def format_user_input(user_input: str) -> str:
     return f"<|start_header_id|>user<|end_header_id|>\n{user_input}<|eot_id|>"
@@ -38,3 +39,27 @@ def load_dataset(dataset_path: str) -> Dataset:
     loader = DPODialogueLoader(dataset_path)
     trl_compatible_dataset = from_loader_to_pref_std_dataset(loader)
     return trl_compatible_dataset
+
+def filter_dataset_mad(dataset, tokenizer, threshold=3.5):
+    chosen_lengths = [len(tokenizer.encode(chosen)) for chosen in dataset["chosen"]]
+    rejected_lengths = [len(tokenizer.encode(rejected)) for rejected in dataset["rejected"]]
+    prompt_lengths = [len(tokenizer.encode(prompt)) for prompt in dataset["prompt"]]
+
+    max_lengths = [max(chosen_lengths[i], rejected_lengths[i], prompt_lengths[i]) for i in range(len(chosen_lengths))]
+
+    # Computing the MAD of the chosen lenghts
+    # https://www.statology.org/modified-z-score/
+    mad = np.median(np.abs(np.median(max_lengths) - np.array(max_lengths)))
+    mod_z_scores = [0.6745 * (x - np.median(max_lengths)) / mad for x in max_lengths]
+    # Ideally we should use eliminate < -3.5 and > 3.5 so it would be np.abs(mod_z_scores) > threshold
+    # But we don't really care about the samples that are too short as they do fit in memory
+    mask = np.array(mod_z_scores) < threshold
+
+    # Creating a new dataset with the filtered samples
+    new_dataset = {
+        "prompt": [dataset["prompt"][i] for i in range(len(dataset["prompt"])) if mask[i]],
+        "chosen": [dataset["chosen"][i] for i in range(len(dataset["chosen"])) if mask[i]],
+        "rejected": [dataset["rejected"][i] for i in range(len(dataset["rejected"])) if mask[i]]
+    }
+
+    return Dataset.from_dict(new_dataset)
